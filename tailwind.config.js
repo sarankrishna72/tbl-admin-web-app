eb23e8743fa96bb3a67b7d187b6ad0dc1e66c342
Sarath <sarath.pr@riskcovry.com>
2024-06-26 01:39:55 -0400

    SIGN IN PAGE INITIAL SETUP

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-700': 'var(--primary-700)',
+        'primary-300': 'var(--primary-300)',
+        'neutral-100': 'var(--neutral-100)'
+      },
+
+    },
   },
   plugins: [],
 }
