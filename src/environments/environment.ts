c04706100e1cf72a2e48028d69965329763650f2
Sarath <sarath.pr@riskcovry.com>
2024-07-23 12:54:27 -0400

    [RESTUARANTR LIST ADDED]

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-   baseURI: "https://api.thebeerlibrary.in",
+   baseURI: "http://ec2-13-232-106-5.ap-south-1.compute.amazonaws.com",
    indexedDbName: "tbl-admin-db-production",
    cipherEncryptionKey: "cHJvZHVjdGlvbkVuY3J5cHRpb24="
 };
