1d818ea2355f0211632ee26a46a2cfe839b4de56
Sarath <sarath.pr@riskcovry.com>
2024-08-02 15:51:51 -0400

    REDIRECTION ISSUE FIXED

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-   baseURI: "http://ec2-13-232-106-5.ap-south-1.compute.amazonaws.com",
+   baseURI: "https://api.bierlibrary.com",
    indexedDbName: "tbl-admin-db-production",
    cipherEncryptionKey: "cHJvZHVjdGlvbkVuY3J5cHRpb24="
 };
