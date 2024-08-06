a8188c7a34029690749c320c9d9a3528ee249ef7
Sarath <sarath.pr@riskcovry.com>
2024-08-06 01:25:33 -0400

    ADDED NEW FIELDS

--- src/app/shared/components/form/components/input-file/input-file.component.ts
+++ src/app/shared/components/form/components/input-file/input-file.component.ts
@@ -68,24 +68,32 @@ export class InputFileComponent extends ControlValueAccessorDirective implements
    */
   prepareFilesList(files: Array<any>) {
     for (const item of files) {
-      this.files.push(item);
+      this.getBase64(item).then((data) => {
+         this.files.push({
+          file: item,
+          url: data,
+          id: null
+         });
+      })
+
     }
   }
 
-  /**
-   * format bytes
-   * @param bytes (File size in bytes)
-   * @param decimals (Decimals point)
-   */
-  formatBytes(bytes: any, decimals  =2) {
-    if (bytes === 0) {
-      return '0 Bytes';
-    }
-    const k = 1024;
-    const dm = decimals <= 0 ? 0 : decimals || 2;
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+
+
+
+  getBase64(file: any) {
+    return new Promise((resolve, reject) => {
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = function () {
+        resolve(reader.result);
+      };
+      reader.onerror = function (error) {
+        reject(error);
+      };
+    })
+
   }
 
 
