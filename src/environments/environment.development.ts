5b484b72a75150c9a1029a390f355256cac88169
Sarath <sarath@flogic.in>
2024-12-04 12:02:14 -0500

    CHANGED URL

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  baseURI: "https://api.thebeerlibrary.in",
+  baseURI: "http://api.thebeerlibrary.in",
   indexedDbName: "tbl-admin-db-development",
   cipherEncryptionKey: "ZGV2ZWxvcG1lbnRFbmNyeXB0aW9u"
 };
