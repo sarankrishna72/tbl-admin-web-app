c519a035b315c1f4a5ac391a972aa89a1c60f4cf
Sarath <sarath@flogic.in>
2025-04-13 01:05:39 -0400

    TBL PHASE 3 CASHIER CHANGES

--- src/app/core/constants/image.ts
+++ src/app/core/constants/image.ts
@@ -1,4 +1,4 @@
-const DEFAULT_IMAGES_PATH = "assets/images"
+const DEFAULT_IMAGES_PATH = "/assets/images"
 export const DEFAULT_IMAGES = {
   "logo": `${DEFAULT_IMAGES_PATH}/logo.svg`,
   "no_data_available": `${DEFAULT_IMAGES_PATH}/no_data_available.svg`,
